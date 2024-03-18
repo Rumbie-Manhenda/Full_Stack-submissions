5c84cba8bae351ad92feaa97294557b4d0f2d58f
Patricia Rumbidzai Manhenda <99598105+Rumbie-Manhenda@users.noreply.github.com>
2024-03-19 01:41:17 +0200

    anecdotes

--- part1/src/unicafe/main.jsx
+++ part1/src/unicafe/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './courseinfor/App.jsx'
+import App from '../unicafe/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render( <App />)
